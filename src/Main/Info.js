0ced9128f9e8fb1bf804de4baf424f412d9f9ef4
Marley Reyna <marley.reyna510@gmail.com>
2022-02-07 21:24:11 -0600

    init

---
+++ src/Main/Info.js
@@ -0,0 +1,26 @@
+import colton from './Images/image-colton.jpg';
+import irene from './Images/image-irene.jpg';
+import anne from './Images/image-anne.jpg';
+
+const custTestimonials = [
+    {
+        image: colton,
+        class: 'colton',
+        name: 'Colton Smith',
+        review: '“ We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! ”'
+    },
+    {
+        image: irene,
+        class: 'irene',
+        name: 'Irene Roberts',
+        review: '“ Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.”'
+    },
+    {
+        image: anne,
+        class: 'anne',
+        name: 'Anne Wallace',
+        review: '“ Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone! ”'
+    },
+]
+
+export default custTestimonials;
---
+++ src/Main/Main.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './Main.scss';
+import custTestimonials from './Info';
+import starIcon from './Images/icon-star.svg';
+
+
+const Main = () => {
+
+    const Stars = [
+        {  
+            class: 'review',
+            text: 'Rated 5 Stars in Reviews'
+        },
+        {  
+            class: 'guru',
+            text: 'Rated 5 Stars in Report Guru'
+        },
+        {  
+            class: 'best-tech',
+            text: 'Rated 5 Stars in BestTech'
+        },
+    ]
+
+    let starsMult = []
+
+    for (let i = 0; i < 5; i++){
+        starsMult.push(<img src={starIcon} alt=''/>)
+    }
+
+    return (
+        <main>
+            <section className='top-section'>
+                <div className='text-box'>
+                    <h1>
+                        10,000+ of our users love our 
+                        products.
+                    </h1>
+                    <p>
+                        We only provide great products 
+                        combined with excellent customer 
+                        service. See what our satisfied 
+                        customers are saying about our 
+                        services.
+                    </p>
+                </div>
+                <div className='stars'>
+                    <ul>
+                        {Stars.map((item, index) => {
+                            return(
+                                <li className={item.class}
+                                key={index}>
+                                    <div>
+                                        {starsMult}
+                                    </div>
+                                    <p>{item.text}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </section>
+            <section className='testimonials'>
+                <ul>
+                    {custTestimonials.map((item, index) =>{
+                        return(
+                            <li className={item.class}
+                            key='index'>
+                                <div>
+                                    <img src={item.image} alt=''/>
+                                    <h2>
+                                        {item.name}
+                                        <span> <br />Verified Buyer</span>
+                                    </h2>
+                                </div>
+                                <p>
+                                    {item.review}
+                                </p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </section>
+        </main>
+    );
+}
+ 
+export default Main;
